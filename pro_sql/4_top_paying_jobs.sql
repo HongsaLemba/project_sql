c0c1d6677fc6ca09b5f3e25a52d188c72c9426d6
HongsaLemba <alamj2607@gmail.com>
2026-09-01 09:48:14 +0530

    commit

--- pro_sql/4_top_paying_jobs.sql
+++ pro_sql/4_top_paying_jobs.sql
@@ -15,9 +15,25 @@ INNER JOIN skills_dim on skills_job_dim.skill_id = skills_dim.skill_id
 WHERE
     job_title_short = 'Data Analyst' AND
     salary_year_avg is not NULL
-    -- AND job_work_from_home = True
+ -- AND job_work_from_home = True
 GROUP BY
     skills
 ORDER BY
     avg_salary DESC
 LIMIT 25;
+
+/*
+Key Takeaways:-
+
+Niche Scarcity Drives the Highest Pay: Blockchain/smart contract development (Solidity, $179k) 
+and specialized databases (Couchbase, $160.5k) command top premiums due to talent scarcity.
+
+Legacy Outlier Distortion: SVN ($400k) is a distinct outlier, 
+typically reflecting rare enterprise/finance legacy systems tied to high-level executive or principal roles.
+
+Deep Learning Convergence: Major AI frameworks (MXNet, Keras, PyTorch, Hugging Face, TensorFlow) 
+cluster tightly between $120k and $149k.
+
+Cloud & CI/CD Infrastructure Premium: Skills bridging analytics with infrastructure (Terraform, VMware, GitLab) 
+consistently surpass traditional BI tools in average compensation. 
+*/
